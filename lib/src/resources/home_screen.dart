35f1fac63b03072218a6c1af961c5d6b6479c62b
yeucon02vn <phiphuqn1@gmail.com>
2019-12-20 22:21:37 +0700

    update homescreen + googlemap

--- lib/src/resources/home_screen.dart
+++ lib/src/resources/home_screen.dart
@@ -1,19 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:taxi_app/src/resources/widgets/home_menu.dart';
+import 'package:taxi_app/src/resources/widgets/ride_picker.dart';
 
 class HomeScreen extends StatefulWidget {
-  @override 
+  @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  var _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       body: Container(
         constraints: BoxConstraints.expand(),
         color: Colors.white,
-        child: Center(child: Text("HOME PAGE"))
-      )
+        child: Stack(
+          children: <Widget>[
+            GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(10.7915178, 106.7271422),
+                zoom: 14.4746,
+              ),
+            ),
+            Positioned(
+              left: 0,
+              top: 0,
+              right: 0,
+              child: Column(
+                children: <Widget>[
+                  AppBar(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0.0,
+                    leading: FlatButton(
+                        onPressed: () {
+                          print("click menu");
+                          _scaffoldKey.currentState.openDrawer();
+                        },
+                        child: Image.asset("ic_menu.png")),
+                    actions: <Widget>[Image.asset("ic_notify.png")],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only( left: 20, right: 20),
+                       child: RidePicker()
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        child: HomeMenu(),
+      ),
     );
   }
 }
